6abd907686489aa613a4fc0d42b2b498235b346d
khrystyna_kozakevych <margaret.gray.777@gmail.com>
2025-03-10 10:06:57 +0100

    update MovieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,12 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import css from './MovieList.module.css';
 
 const MovieList = ({ movies }) => {
+  const location = useLocation();
+
   return (
     <div className={css.list}>
       {movies.map(({ id, poster_path, title }) => (
         <div key={id} className={css.movie}>
-          <Link to={`/movies/${id}`}>
+          <Link to={`/movies/${id}`} state={{ from: location }}>
             <img
               src={`https://image.tmdb.org/t/p/w500${poster_path}`}
               alt={title}
